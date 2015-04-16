e186c261a9460314caa1af6046a783d4c95317be
jenkins <jenkinsmail@bspitsm.com>
2015-04-16 09:22:34 +0900

    comment

---
+++ Calc.php
@@ -0,0 +1,8 @@
+<?php
+class Calc
+{
+    public function add($a, $b)
+    {
+        return $a + $b;
+    }
+}
---
+++ CalcTest.php
@@ -0,0 +1,12 @@
+<?php
+require_once 'PHPUnit/Framework.php';
+require_once './Calc.php';
+
+class CalcTest extends PHPUnit_Framework_TestCase
+{
+    public function testAdd()
+    {   
+        $calc = new Calc;
+        $this->assertEquals(2, $calc->add(1, 1));
+    }   
+}
